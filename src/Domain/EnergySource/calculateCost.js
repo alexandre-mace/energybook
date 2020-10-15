04171d05bdf599b64b78cc8103e232dd64dcdd26
alexandre-mace <contact.alexandremace@gmail.com>
2020-10-16 00:51:59 +0200

    Remove log

--- src/Domain/EnergySource/calculateCost.js
+++ src/Domain/EnergySource/calculateCost.js
@@ -3,37 +3,6 @@ import calculateEnergySourceCost from "./calculateEnergySourceCost";
 import calculateEnergySourcePower from "./calculateEnergySourcePower";
 
 const calculateCost = (eolNb, nucNb, solNb, therCoalNb, therOilNb, therGasNb, hydroNb) => {
-    console.log(calculateEnergySourcePower(solNb, powerData.sol.averagePower, powerData.sol.averageChargeFactor))
-    console.log((
-        // calculateEnergySourceCost(
-        //     calculateEnergySourcePower(eolNb, powerData.eol.averagePower, powerData.eol.averageChargeFactor),
-        //     powerData.eol.cost
-        // ) +
-        // calculateEnergySourceCost(
-        //     calculateEnergySourcePower(nucNb, powerData.nuc.averagePower, powerData.nuc.averageChargeFactor),
-        //     powerData.nuc.cost
-        // ) +
-        calculateEnergySourceCost(
-            calculateEnergySourcePower(solNb, powerData.sol.averagePower, powerData.sol.averageChargeFactor),
-            powerData.sol.cost
-        )
-        // calculateEnergySourceCost(
-        //     calculateEnergySourcePower(therCoalNb, powerData.therCoal.averagePower, powerData.therCoal.averageChargeFactor),
-        //     powerData.therCoal.cost
-        // )
-        // calculateEnergySourceCost(
-        //     calculateEnergySourcePower(therOilNb, powerData.therOil.averagePower, powerData.therOil.averageChargeFactor),
-        //     powerData.therOil.cost
-        // ) +
-        // calculateEnergySourceCost(
-        //     calculateEnergySourcePower(therGasNb, powerData.therGas.averagePower, powerData.therGas.averageChargeFactor),
-        //     powerData.therGas.cost
-        // ) +
-        // calculateEnergySourceCost(
-        //     calculateEnergySourcePower(hydroNb, powerData.hydro.averagePower, powerData.hydro.averageChargeFactor),
-        //     powerData.hydro.cost
-        // )
-    ) )
     return (
         (
             calculateEnergySourceCost(
