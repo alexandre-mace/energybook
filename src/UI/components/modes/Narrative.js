ed31f4652ee573006823762c355949b25b53280d
alexandre-mace <contact.alexandremace@gmail.com>
2020-10-30 19:46:21 +0100

    Update narrative section

--- src/UI/components/modes/Narrative.js
+++ src/UI/components/modes/Narrative.js
@@ -48,6 +48,7 @@ function Narrative() {
                 <div className="container my-3 my-md-5">
                     <div className="row">
                         <div className="col">
+                            <p>As you can see, the energy consumption is growing.</p>
                             <p>Thanks to energy we can produce the amazing thing that
                                 is <strong>electricity</strong> <span role={"img"} aria-label={"thunder"}>&#9889;</span>.
                             </p>
@@ -62,9 +63,11 @@ function Narrative() {
                 <div className="container my-3 my-md-5">
                     <div className="row">
                         <div className="col">
-                            <p>So yes, <strong>energy</strong> is cool</p>
+                            <p>So yes, <strong>energy</strong> is cool.</p>
                             <p>But there is a big issue currently highly coupled with energy consumption, <strong>co2
                                 emissions</strong>. 🏭</p>
+                            <p>Carbon dioxide (CO2) is the primary greenhouse gas emitted through human activities. Co2 emissions are carbon dioxide that planes, cars, factories, etc. produce, thought to be harmful to the environment.</p>
+                            <p>Carbon dioxide (CO2) is released into Earth’s atmosphere mostly by the burning of carbon-containing fuels and the decay of wood and other plant matter. Under all conditions found naturally on Earth, CO2 is an invisible, odorless gas. It is removed from the atmosphere mostly by plants, which extract carbon from CO2 to build their tissues, and by the oceans, in which CO2 dissolves.</p>
                         </div>
                     </div>
                 </div>
@@ -74,11 +77,12 @@ function Narrative() {
                 <div className="container my-3 my-md-5">
                     <div className="row">
                         <div className="col">
-                            <p>PS : Note the weird trick, since we started looking after co2 emissions in the 90's, we
+                            <p>It’s widely recognised that to avoid the worst impacts of climate change, the world needs to urgently reduce emissions.</p>
+                            <p>Note the weird trick, since we started looking after co2 emissions in the 90's, we
                                 took
                                 co2 emissions to another planet. </p>
                             <p>How can we <i>partly</i> solve it ?</p>
-                            <p>By using <strong>renewable energy</strong> (and decarbonized energy)</p>
+                            <p>By using <strong>renewable energy</strong> (and decarbonized energy).</p>
                         </div>
                     </div>
                 </div>
@@ -88,6 +92,7 @@ function Narrative() {
                 <div className="container my-3 my-md-5">
                     <div className="row">
                         <div className="col">
+                            <p>Renewable energy is energy that is collected from renewable resources, which are naturally replenished on a human timescale, such as sunlight, wind, rain, tides, waves, and geothermal heat.</p>
                             <p>That is definetely not a lot of <strong>renewables</strong>, lets see what is in there...
                             </p>
                         </div>
@@ -101,6 +106,7 @@ function Narrative() {
                         <div className="col">
                             <p>And, let's take a deeper look at who are the <strong>non renewables</strong>, the biggest
                                 part of energy consumption source.</p>
+                            <p>A non-renewable resource (also called a finite resource) is a natural resource that cannot be readily replaced by natural means at a quick enough pace to keep up with consumption.</p>
                         </div>
                     </div>
                 </div>
@@ -152,6 +158,7 @@ function Narrative() {
                 <div className="container my-3 my-md-5">
                     <div className="row">
                         <div className="col">
+                            <p>The graphic speaks for itself.</p>
                             <p>Also, the world has changed his mind on which technology to invest deeper overtime.</p>
                         </div>
                     </div>
