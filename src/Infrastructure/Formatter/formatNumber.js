304a590581ed8179e91d2f34491ae5d45d09a5db
alexandre-mace <contact.alexandremace@gmail.com>
2020-11-04 01:11:01 +0100

    Update responsive

--- src/Infrastructure/Formatter/formatNumber.js
+++ src/Infrastructure/Formatter/formatNumber.js
@@ -1,4 +1,8 @@
 function formatNumber(num) {
-    return num.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ')
+    return num
+        .toFixed(2)
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ')
+        .replace(/\.0+$/,'')
 }
 export default formatNumber;
